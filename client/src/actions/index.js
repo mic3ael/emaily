097334f11b0f7d18c8a8a095190ae13f224fb29a
Michael Horojanski <mic3ael@gmail.com>
2017-08-15 12:37:19 +0300

    first commit

---
+++ client/src/actions/index.js
@@ -0,0 +1,12 @@
+import axios from 'axios';
+import {FETCH_USER} from './types';
+
+export const fetchUser = () => async dispatch => dispatch({
+  type: FETCH_USER,
+  payload: (await axios.get('/api/current_user')).data
+});
+
+export const handleToken = token => async dispatch => dispatch({
+  type: FETCH_USER,
+  payload: (await axios.post('/api/stripe', token)).data
+});
